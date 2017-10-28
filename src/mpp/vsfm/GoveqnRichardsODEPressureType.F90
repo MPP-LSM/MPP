MPP-LSM/MPP

commit ecf48f2bf3e0f3f663c5696e575aea178f3e1431

Adds export of mass flux for boundary and source-sink conditions in VSFM

src/mpp/vsfm/GoveqnRichardsODEPressureType.F90
@@ -29,6 +29,7 @@ module GoveqnRichardsODEPressureType
 
      PetscReal                             , pointer :: internal_flux(:)          ! mass flux betwenn internal connections [kg/s]
      PetscReal                             , pointer :: boundary_flux(:)          ! mass flux between boundary connections [kg/s]
+     PetscReal                             , pointer :: ss_flux(:)                ! mass flux for source-sink connections [kg/s]
      PetscReal                             , pointer :: lat_mass_exc(:)           ! lateral mass exchanged [kg]
      PetscReal                             , pointer :: bnd_mass_exc(:)           ! mass exchanged through boundary conditions [kg]
 
@@ -68,6 +69,7 @@ module GoveqnRichardsODEPressureType
      procedure, public :: GetNumConditions          => RichardsODEPressureGetNumConditions
      procedure, public :: GetNumCellsInConditions   => RichardsODEPressureGetNumCellsInConditions
      procedure, public :: SetSOEAuxVarOffsets       => RichardsODEPressureSetSOEAuxVarOffsets
+     procedure, public :: GetNumInternalConnections => RichardsODEGetNumInternalConnections
      procedure, public :: CreateVectors             => RichardsODEPressureCreateVectors
 
      procedure, public :: ComputeLateralFlux        => RichardsODEComputeLateralFlux
@@ -190,7 +192,11 @@ contains
        ncond       = ncond + 1
        cur_cond => cur_cond%next
     enddo
-    allocate(this%aux_vars_ss(ncells_cond))
+    allocate(this%aux_vars_ss    (ncells_cond))
+    allocate(this%ss_flux        (ncells_cond))
+
+    this%ss_flux(:) = 0.d0
+
     do icond = 1,ncells_cond
        call this%aux_vars_ss(icond)%Init()
     enddo
@@ -922,6 +928,7 @@ contains
     use MultiPhysicsProbConstants    , only : AUXVAR_INTERNAL
     use MultiPhysicsProbConstants    , only : AUXVAR_BC
     use MultiPhysicsProbConstants    , only : AUXVAR_SS
+    use MultiPhysicsProbConstants    , only : AUXVAR_CONN_INTERNAL
     use MultiPhysicsProbConstants    , only : VAR_LIQ_SAT
     use MultiPhysicsProbConstants    , only : VAR_MASS
     use MultiPhysicsProbConstants    , only : VAR_SOIL_MATRIX_POT
@@ -932,6 +939,7 @@ contains
     use MultiPhysicsProbConstants    , only : CONN_HORIZONTAL
     use MultiPhysicsProbConstants    , only : COND_BC
     use MultiPhysicsProbConstants    , only : COND_DIRICHLET_FRM_OTR_GOVEQ
+    use MultiPhysicsProbConstants    , only : COND_DOWNREGULATE_POT_MASS_RATE
     use SystemOfEquationsVSFMAuxType , only : sysofeqns_vsfm_auxvar_type
     use ConditionType                , only : condition_type
     use ConnectionSetType            , only : connection_set_type
@@ -1027,6 +1035,31 @@ contains
 
        num_avars_set = size(ge_avars)
 
+    case (AUXVAR_CONN_INTERNAL)
+       ge_avars => this%aux_vars_in
+
+       ! Interior cells
+       cur_conn_set => this%mesh%intrn_conn_set_list%first
+       sum_conn = 0
+       do
+          if (.not.associated(cur_conn_set)) exit
+
+          if ( sum_conn + cur_conn_set%num_connections + iauxvar_off > size(soe_avars)) then
+             write(iulog,*) 'size(soe_avars) is smaller than number of internal connections'
+             call endrun(msg=errMsg(__FILE__, __LINE__))
+          endif
+
+          do iconn = 1, cur_conn_set%num_connections
+             sum_conn = sum_conn + 1
+
+             soe_avars(iconn + iauxvar_off)%mass_flux = this%internal_flux(sum_conn)
+
+          enddo
+          cur_conn_set => cur_conn_set%next
+       enddo
+
+       num_avars_set = sum_conn
+
     case (AUXVAR_SS)
 
        condition_id = 0
@@ -1065,13 +1098,36 @@ contains
                 sum_conn = sum_conn + 1
                 select case(cur_cond%itype)
                 case (COND_MASS_RATE)
-                   soe_avars(iconn + iauxvar_off)%condition_value = cur_cond%value(iconn)
+                   soe_avars(sum_conn + iauxvar_off)%condition_value = cur_cond%value(iconn)
+                   soe_avars(sum_conn + iauxvar_off)%mass_flux       = this%ss_flux(sum_conn)
+
                 case default
                    write(string,*) cur_cond%itype
                    write(iulog,*) 'Unknown cur_cond%itype = ' // trim(string)
                    call endrun(msg=errMsg(__FILE__, __LINE__))
                 end select
              enddo
+
+          else
+
+             cur_conn_set => cur_cond%conn_set
+             do iconn = 1, cur_conn_set%num_connections
+
+                sum_conn = sum_conn + 1
+                select case(cur_cond%itype)
+                case (COND_MASS_RATE)
+                   soe_avars(sum_conn + iauxvar_off)%mass_flux = this%ss_flux(sum_conn)
+
+                case (COND_DOWNREGULATE_POT_MASS_RATE)
+                   soe_avars(sum_conn + iauxvar_off)%mass_flux = this%ss_flux(sum_conn)
+
+                case default
+                   write(string,*) cur_cond%itype
+                   write(iulog,*) 'Unknown cur_cond%itype = ' // trim(string)
+                   call endrun(msg=errMsg(__FILE__, __LINE__))
+                end select
+             enddo
+
           endif
 
           cur_cond => cur_cond%next
@@ -1110,6 +1166,7 @@ contains
              soe_avars(sum_conn + iauxvar_off)%boundary_mass_exchanged = &
                   this%bnd_mass_exc(sum_conn)
 
+             soe_avars(sum_conn + iauxvar_off)%mass_flux = this%boundary_flux(sum_conn)
           enddo
           cur_cond => cur_cond%next
        enddo
@@ -1722,6 +1779,8 @@ contains
           case (COND_MASS_RATE)
              ff(cell_id) = ff(cell_id) - cur_cond%value(iconn)/FMWH2O
 
+             this%ss_flux(sum_conn) = cur_cond%value(iconn)
+
           case (COND_DOWNREGULATE_POT_MASS_RATE)
              dP          = this%aux_vars_in(cell_id)%pressure - PRESSURE_REF
              Pc          = this%aux_vars_ss(sum_conn)%pot_mass_sink_pressure
@@ -1734,6 +1793,8 @@ contains
              endif
 
              ff(cell_id) = ff(cell_id) - cur_cond%value(iconn)/factor/FMWH2O
+
+             this%ss_flux(sum_conn) = cur_cond%value(iconn)/factor
 
           case default
             write(iulog,*)'RichardsODEPressureDivergence: Unknown cond_type in SS.'
@@ -3137,6 +3198,38 @@ contains
     end select
 
   end subroutine RichardsODESetAuxVarConnIntValue
+
+  !------------------------------------------------------------------------
+  subroutine RichardsODEGetNumInternalConnections(this, nconn_in)
+    !
+    ! !DESCRIPTION:
+    ! Returns the number of internal connection
+    !
+    ! !USES:
+    use ConnectionSetType         , only : connection_set_type
+    !
+    implicit none
+    !
+    ! !ARGUMENTS
+    class(goveqn_richards_ode_pressure_type) :: this
+    PetscInt, intent(out)                    :: nconn_in
+    !
+    !
+    ! !LOCAL VARAIABLES:
+    type(connection_set_type) , pointer      :: cur_conn_set
+
+    ! Interior cells
+    cur_conn_set => this%mesh%intrn_conn_set_list%first
+    nconn_in = 0
+    do
+       if (.not.associated(cur_conn_set)) exit
+
+       nconn_in = nconn_in + cur_conn_set%num_connections
+
+       cur_conn_set => cur_conn_set%next
+    end do
+
+  end subroutine RichardsODEGetNumInternalConnections
 
 #endif
 

src/mpp/util/MultiPhysicsProbConstants.F90
@@ -120,6 +120,7 @@ module MultiPhysicsProbConstants
   PetscInt, parameter, public :: VAR_PRESSURE_DN                   = 641
   PetscInt, parameter, public :: VAR_CAMPBELL_HE                   = 642
   PetscInt, parameter, public :: VAR_CAMPBELL_N                    = 643
+  PetscInt, parameter, public :: VAR_MASS_FLUX                     = 644
 
   !
   PetscInt, parameter, public :: AUXVAR_INTERNAL                   = 701

src/mpp/vsfm/MultiPhysicsProbVSFM.F90
@@ -1178,11 +1178,13 @@ contains
     PetscInt                               :: igoveqn
     PetscInt                               :: num_bc
     PetscInt                               :: num_ss
+    PetscInt                               :: nconn_in
     PetscInt                               :: icond
     PetscInt                               :: iauxvar
     PetscInt                               :: iauxvar_beg, iauxvar_end
     PetscInt                               :: iauxvar_beg_bc, iauxvar_end_bc
     PetscInt                               :: iauxvar_beg_ss, iauxvar_end_ss
+    PetscInt                               :: iauxvar_beg_conn_in, iauxvar_end_conn_in
     PetscInt                               :: count_bc, count_ss
     PetscInt                               :: offset_bc, offset_ss
     PetscInt, pointer                      :: ncells_for_bc(:)
@@ -1206,6 +1208,7 @@ contains
     !         and SSs for all governing equations
     !
     igoveqn = 0
+    soe%num_auxvars_conn_in = 0
     cur_goveq => soe%goveqns
     do
        if (.not.associated(cur_goveq)) exit
@@ -1217,6 +1220,8 @@ contains
                COND_DIRICHLET_FRM_OTR_GOVEQ, num_bc, ncells_for_bc)
           call cur_goveq%GetNumCellsInConditions(COND_SS, -9999, &
                num_ss, ncells_for_ss)
+          call cur_goveq%GetNumInternalConnections(nconn_in)
+          soe%num_auxvars_conn_in = soe%num_auxvars_conn_in + nconn_in
 
        end select
 
@@ -1245,6 +1250,7 @@ contains
     allocate(soe%aux_vars_in           (soe%num_auxvars_in))
     allocate(soe%aux_vars_bc           (soe%num_auxvars_bc))
     allocate(soe%aux_vars_ss           (soe%num_auxvars_ss))
+    allocate(soe%aux_vars_conn_in      (soe%num_auxvars_conn_in))
 
     allocate(soe%soe_auxvars_bc_offset (soe%num_auxvars_bc))
     allocate(soe%soe_auxvars_ss_offset (soe%num_auxvars_ss))
@@ -1261,7 +1267,9 @@ contains
     count_bc       = 0
     count_ss       = 0
     offset_bc      = 0
-    offset_ss      = 0
+    offset_ss      = 0    
+    iauxvar_beg_conn_in    = 0
+    iauxvar_end_conn_in    = 0
 
     !
     ! Pass-2: Set values for auxvars of SoE
@@ -1276,6 +1284,7 @@ contains
                COND_DIRICHLET_FRM_OTR_GOVEQ, num_bc, ncells_for_bc)
           call cur_goveq%GetNumCellsInConditions(COND_SS, -9999, &
                num_ss, ncells_for_ss)
+          call cur_goveq%GetNumInternalConnections(nconn_in)
 
        end select
 
@@ -1290,6 +1299,17 @@ contains
           soe%aux_vars_in(iauxvar)%is_in     = PETSC_TRUE
           soe%aux_vars_in(iauxvar)%goveqn_id = igoveqn
        enddo
+
+       iauxvar_beg_conn_in = iauxvar_end_conn_in + 1
+       iauxvar_end_conn_in = iauxvar_end_conn_in + nconn_in
+
+       do iauxvar = iauxvar_beg_conn_in, iauxvar_end_conn_in
+          call soe%aux_vars_conn_in(iauxvar)%Init()
+
+          soe%aux_vars_conn_in(iauxvar)%is_in = PETSC_TRUE
+          soe%aux_vars_conn_in(iauxvar)%goveqn_id = igoveqn
+
+       end do
 
        allocate(offsets_bc(num_bc))
        allocate(offsets_ss(num_ss))
@@ -1346,6 +1366,10 @@ contains
 
        cur_goveq => cur_goveq%next
     enddo
+
+    do iauxvar = 1, soe%num_auxvars_conn_in
+       call soe%aux_vars_conn_in(iauxvar)%Init()
+    end do
 
   end subroutine VSFMMPPAllocateAuxVars
 

src/mpp/vsfm/SystemOfEquationsVSFMAuxType.F90
@@ -30,6 +30,7 @@ module SystemOfEquationsVSFMAuxType
      PetscReal :: condition_value        ! [depends on the type of condition]
      PetscReal :: lateral_mass_exchanged ! [kg]
      PetscReal :: boundary_mass_exchanged! [kg]
+     PetscReal :: mass_flux              ! [kg/s]
 
      PetscInt  :: goveqn_id              ! [-]
      PetscInt  :: condition_id           ! [-]
@@ -69,6 +70,7 @@ contains
     this%condition_value         = 0.d0
     this%lateral_mass_exchanged  = 0.d0
     this%boundary_mass_exchanged = 0.d0
+    this%mass_flux               = 0.d0
     this%goveqn_id               = 0
     this%condition_id            = 0
 
@@ -142,6 +144,7 @@ contains
     use MultiPhysicsProbConstants, only : VAR_SOIL_MATRIX_POT
     use MultiPhysicsProbConstants, only : VAR_LATERAL_MASS_EXCHANGED
     use MultiPhysicsProbConstants, only : VAR_BC_MASS_EXCHANGED
+    use MultiPhysicsProbConstants, only : VAR_MASS_FLUX
     !
     implicit none
     !
@@ -171,6 +174,8 @@ contains
        variable_value = this%lateral_mass_exchanged
     case (VAR_BC_MASS_EXCHANGED)
        variable_value = this%boundary_mass_exchanged
+    case (VAR_MASS_FLUX)
+       variable_value = this%mass_flux
     case default
        write(iulog,*) 'In VSFMSOEAuxVarGetValue: unknown var_type'
        call endrun(msg=errMsg(__FILE__, __LINE__))

src/mpp/vsfm/SystemOfEquationsVSFMType.F90
@@ -31,6 +31,9 @@ module SystemOfEquationsVSFMType
      type (sysofeqns_vsfm_auxvar_type), pointer :: aux_vars_in(:)            ! Internal state.
      type (sysofeqns_vsfm_auxvar_type), pointer :: aux_vars_bc(:)            ! Boundary conditions.
      type (sysofeqns_vsfm_auxvar_type), pointer :: aux_vars_ss(:)            ! Source-sink.
+     type (sysofeqns_vsfm_auxvar_type), pointer :: aux_vars_conn_in(:)       ! Internal connections
+
+     
      PetscInt, pointer                          :: soe_auxvars_bc_offset (:) ! Cummulative sum of number of control volumes associated with each boundary condition.
      PetscInt, pointer                          :: soe_auxvars_ss_offset (:) ! Cummulative sum of number of control volumes associated with each source-sink condition.
      PetscInt, pointer                          :: soe_auxvars_bc_ncells (:) ! Number of control volumes associated with each boundary condition.
@@ -39,6 +42,7 @@ module SystemOfEquationsVSFMType
      PetscInt                                   :: num_auxvars_in_local      ! Number of auxvars associated with internal state.
      PetscInt                                   :: num_auxvars_bc            ! Number of auxvars associated with boundary condition.
      PetscInt                                   :: num_auxvars_ss            ! Number of auxvars associated with source-sink condition.
+     PetscInt                                   :: num_auxvars_conn_in       ! Number of auxvars associated with internal connections
    contains
      procedure, public :: Init                   => VSFMSOEInit
      procedure, public :: Setup                  => VSFMSOESetup
@@ -88,6 +92,7 @@ contains
     nullify(this%aux_vars_in           )
     nullify(this%aux_vars_bc           )
     nullify(this%aux_vars_ss           )
+    nullify(this%aux_vars_conn_in      )
 
     nullify(this%soe_auxvars_bc_offset )
     nullify(this%soe_auxvars_ss_offset )
@@ -184,6 +189,7 @@ contains
     PetscInt                                          :: offset
     PetscInt                                          :: num_bc
     PetscInt                                          :: num_ss
+    PetscInt                                          :: nconn_in
     PetscInt                                          :: total_ncells_for_bc
     PetscInt                                          :: total_ncells_for_ss
     PetscInt, pointer                                 :: ncells_for_bc(:)
@@ -350,6 +356,15 @@ contains
        call vsfm_soe%aux_vars_in(iauxvar)%Init()
        vsfm_soe%aux_vars_in(iauxvar)%is_in      = PETSC_TRUE
        vsfm_soe%aux_vars_in(iauxvar)%goveqn_id  = 1
+    enddo
+
+    call goveq_richards_pres%GetNumInternalConnections(nconn_in)
+
+    allocate(vsfm_soe%aux_vars_conn_in(nconn_in))
+    do iauxvar = 1, nconn_in
+       call vsfm_soe%aux_vars_conn_in(iauxvar)%Init()
+       vsfm_soe%aux_vars_conn_in(iauxvar)%is_in      = PETSC_TRUE
+       vsfm_soe%aux_vars_conn_in(iauxvar)%goveqn_id  = 1
     enddo
 
     call goveq_richards_pres%NumCellsInConditions( &
@@ -955,6 +970,7 @@ contains
     use MultiPhysicsProbConstants     , only : AUXVAR_INTERNAL
     use MultiPhysicsProbConstants     , only : AUXVAR_BC
     use MultiPhysicsProbConstants     , only : AUXVAR_SS
+    use MultiPhysicsProbConstants     , only : AUXVAR_CONN_INTERNAL
     use GoverningEquationBaseType     , only : goveqn_base_type
     use GoveqnRichardsODEPressureType , only : goveqn_richards_ode_pressure_type
     !
@@ -967,13 +983,17 @@ contains
     class(goveqn_base_type),pointer :: cur_goveq
     PetscInt                        :: iauxvar_in_off
     PetscInt                        :: iauxvar_bc_off
+    PetscInt                        :: iauxvar_ss_off
+    PetscInt                        :: iauxvar_conn_in_off
     PetscInt                        :: num_auxvars_filled
     PetscErrorCode                  :: ierr
 
     call VecCopy(this%soln, this%soln_prev,ierr); CHKERRQ(ierr)
 
-    iauxvar_in_off  = 0
-    iauxvar_bc_off  = 0
+    iauxvar_in_off       = 0
+    iauxvar_bc_off       = 0
+    iauxvar_ss_off       = 0
+    iauxvar_conn_in_off  = 0
 
     select case (this%itype)
     case(SOE_RE_ODE)
@@ -992,7 +1012,16 @@ contains
                      iauxvar_bc_off, num_auxvars_filled)
                 iauxvar_bc_off = iauxvar_bc_off + num_auxvars_filled
 
-          end select
+                call cur_goveq%SetDataInSOEAuxVar(AUXVAR_SS      , this%aux_vars_ss, &
+                     iauxvar_ss_off, num_auxvars_filled)
+                iauxvar_ss_off = iauxvar_ss_off + num_auxvars_filled
+
+                call cur_goveq%SetDataInSOEAuxVar(AUXVAR_CONN_INTERNAL, this%aux_vars_conn_in, &
+                     iauxvar_conn_in_off, num_auxvars_filled)
+                iauxvar_conn_in_off = iauxvar_conn_in_off + num_auxvars_filled
+
+
+             end select
           cur_goveq => cur_goveq%next
        enddo
 
@@ -1131,6 +1160,7 @@ contains
     use MultiPhysicsProbConstants, only : AUXVAR_INTERNAL
     use MultiPhysicsProbConstants, only : AUXVAR_BC
     use MultiPhysicsProbConstants, only : AUXVAR_SS
+    use MultiPhysicsProbConstants, only : AUXVAR_CONN_INTERNAL
     !
     implicit none
     !
@@ -1162,6 +1192,10 @@ contains
        auxvars      => this%aux_vars_bc
        iauxvar_off  = this%soe_auxvars_bc_offset(soe_auxvar_id)
        nauxvar      = this%soe_auxvars_bc_ncells(soe_auxvar_id)
+    case(AUXVAR_CONN_INTERNAL)
+       auxvars      => this%aux_vars_conn_in
+       iauxvar_off  = 0
+       nauxvar      = this%num_auxvars_conn_in
     case default
        write(iulog,*) 'VSFMSOEGetDataForCLM: Unknown soe_auxvar_type'
        call endrun(msg=errMsg(__FILE__, __LINE__))

commit d778309807994dc14b52e0dd2e54bd805ce0a978

Fixes the memory leak introduced in ecf48f2
Value of `iauxvar_off` includes cumulative offset value thus one needs to use
`iconn` instead of `sum_conn`.

src/mpp/vsfm/GoveqnRichardsODEPressureType.F90
@@ -1098,8 +1098,8 @@ contains
                 sum_conn = sum_conn + 1
                 select case(cur_cond%itype)
                 case (COND_MASS_RATE)
-                   soe_avars(sum_conn + iauxvar_off)%condition_value = cur_cond%value(iconn)
-                   soe_avars(sum_conn + iauxvar_off)%mass_flux       = this%ss_flux(sum_conn)
+                   soe_avars(iauxvar + iauxvar_off)%condition_value = cur_cond%value(iconn)
+                   soe_avars(iauxvar + iauxvar_off)%mass_flux       = this%ss_flux(sum_conn)
 
                 case default
                    write(string,*) cur_cond%itype
@@ -1116,10 +1116,10 @@ contains
                 sum_conn = sum_conn + 1
                 select case(cur_cond%itype)
                 case (COND_MASS_RATE)
-                   soe_avars(sum_conn + iauxvar_off)%mass_flux = this%ss_flux(sum_conn)
+                   soe_avars(iauxvar + iauxvar_off)%mass_flux = this%ss_flux(sum_conn)
 
                 case (COND_DOWNREGULATE_POT_MASS_RATE)
-                   soe_avars(sum_conn + iauxvar_off)%mass_flux = this%ss_flux(sum_conn)
+                   soe_avars(iauxvar + iauxvar_off)%mass_flux = this%ss_flux(sum_conn)
 
                 case default
                    write(string,*) cur_cond%itype
@@ -1163,10 +1163,10 @@ contains
              this%bnd_mass_exc(sum_conn) = this%bnd_mass_exc(sum_conn) + &
                   this%boundary_flux(sum_conn)*this%dtime
 
-             soe_avars(sum_conn + iauxvar_off)%boundary_mass_exchanged = &
+             soe_avars(iconn + iauxvar_off)%boundary_mass_exchanged = &
                   this%bnd_mass_exc(sum_conn)
 
-             soe_avars(sum_conn + iauxvar_off)%mass_flux = this%boundary_flux(sum_conn)
+             soe_avars(iconn + iauxvar_off)%mass_flux = this%boundary_flux(sum_conn)
           enddo
           cur_cond => cur_cond%next
        enddo