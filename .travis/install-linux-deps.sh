04fd45415345af5b1a902ee7d31a6e94dbb9b4be
Gautam Bisht <gbisht@lbl.gov>
2018-01-17 16:16:28 -0800

    Adds support for Travis-CI

---
+++ .travis/install-linux-deps.sh
@@ -0,0 +1,5 @@
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update -qq
+#sudo apt-get install -y cmake gcc gfortran g++ liblapack-dev libopenmpi-dev openmpi-bin
+sudo apt-get install -y cmake gcc gfortran g++
+
