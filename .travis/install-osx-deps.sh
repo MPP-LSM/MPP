210f8ec65afcd8289ffe35fbf33213456f9a130e
Gautam Bisht <gbisht@lbl.gov>
2018-04-03 16:14:25 -0700

    Enable Travis-CI testing on os x

--- .travis/install-osx-deps.sh
+++ .travis/install-osx-deps.sh
@@ -1,14 +1,15 @@
 # Install required software
 brew update
+brew cask uninstall --force oclint
 brew install git
 brew upgrade cmake
 brew tap homebrew/science
-brew unlink gcc
-brew install gcc
+#brew unlink gcc
+brew install gcc@7
 #brew install open-mpi
-brew install netcdf
+#brew install netcdf
 
 # Make sure the weird gfortran library links are in place.
-ln -s /usr/local/lib/gcc/5/libgfortran.dylib /usr/local/lib/libgfortran.dylib
-ln -s /usr/local/lib/gcc/5/libgfortran.a /usr/local/lib/libgfortran.a
+#ln -s /usr/local/lib/gcc/5/libgfortran.dylib /usr/local/lib/libgfortran.dylib
+#ln -s /usr/local/lib/gcc/5/libgfortran.a /usr/local/lib/libgfortran.a
 
