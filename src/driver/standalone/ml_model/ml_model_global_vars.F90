7349c99575e094f9978e882b9042b30e75cd6f2c
Gautam Bisht <gautam.bisht@pnnl.gov>
2020-07-28 16:33:50 -0700

    Initial checkin of ML model demo

---
+++ src/driver/standalone/ml_model/ml_model_global_vars.F90
@@ -0,0 +1,39 @@
+module ml_model_global_vars
+
+  implicit none
+
+#include <petsc/finclude/petsc.h>
+
+  PetscInt  , parameter :: nx       = 1
+  PetscInt  , parameter :: ny       = 1
+  PetscReal , parameter :: x        = 1.d0
+  PetscReal , parameter :: y        = 1.d0
+  PetscReal , parameter :: z_cair   = 46.d0
+  PetscReal , parameter :: z_cleaf  = 46.d0
+  PetscInt  , parameter :: nz_cair  = 92
+  PetscInt  , parameter :: nz_cleaf = 92
+
+  PetscReal , parameter :: hc       = 21.d0
+
+  PetscInt, parameter :: nbot = 6
+  PetscInt, parameter :: ntop = 42
+
+  PetscInt  :: ncair
+  PetscInt  :: ntree
+
+  PetscInt :: CAIR_MESH
+  PetscInt :: CLEF_MESH
+  PetscInt :: SOIL_MESH
+
+  PetscInt :: CAIR_TEMP_GE
+  PetscInt :: CAIR_VAPR_GE
+  PetscInt :: CLEF_TEMP_SUN_GE
+  PetscInt :: CLEF_TEMP_SHD_GE
+  PetscInt :: SOIL_TEMP_GE
+  PetscInt :: SOIL_VAPR_GE
+
+  PetscInt :: CLEF_REGION_IN_CAIR_MESH
+  PetscInt :: CAIR_REGION_IN_CLEF_MESH
+
+end module ml_model_global_vars
+
---
+++ src/driver/standalone/ml_model/ml_model_problem.F90
@@ -0,0 +1,101 @@
+module ml_model_problem
+
+  use mpp_varctl                     , only : iulog
+  use mpp_abortutils                 , only : endrun
+  use mpp_shr_log_mod                , only : errMsg => shr_log_errMsg
+  use MultiPhysicsProbShortwave      , only : mpp_shortwave_type
+  use MultiPhysicsProbLongwave       , only : mpp_longwave_type
+  use MultiPhysicsProbLBL            , only : mpp_lbl_type
+  use MultiPhysicsProbPhotosynthesis , only : mpp_photosynthesis_type
+  use MultiPhysicsProbMLC            , only : mpp_mlc_type
+  use ml_model_global_vars
+  use petscsys
+  use petscdm
+  use petscdmda
+
+  implicit none
+
+  type(mpp_shortwave_type)      :: shortwave_mpp
+  type(mpp_longwave_type)       :: longwave_mpp
+  type(mpp_lbl_type)            :: lbl_mpp
+  type(mpp_photosynthesis_type) :: phtsyn_mpp
+  type(mpp_mlc_type)            :: mlc_mpp
+
+#include <petsc/finclude/petsc.h>
+
+  public :: run_ml_model_problem
+
+contains
+
+  !------------------------------------------------------------------------
+  subroutine read_namelist_file(namelist_filename)
+    !
+    implicit none
+    !
+    character(len=256), optional :: namelist_filename
+    !
+    character(len=256)           :: ioerror_msg
+    character(len=2560)          :: namelist_buffer
+    integer                      :: nml_unit, nml_error
+
+    namelist / problem_options / ncair, ntree
+
+    if (present(namelist_filename)) then
+       nml_unit = 16
+       open(unit=nml_unit, file=trim(namelist_filename), action='read', access='stream', &
+            form='unformatted', iostat=nml_error)
+       if (nml_error /= 0) then
+          write(*,*)'ERROR: Unable to open namelist file: ',trim(namelist_filename)
+          call exit(-1)
+       endif
+
+       read(unit=nml_unit, iostat=nml_error, iomsg=ioerror_msg) namelist_buffer
+       if (.not. is_iostat_end(nml_error)) then
+          write(*,*)"ERROR: Unable to read '",trim(namelist_filename),"' till EOF"
+          call exit(-1)
+       endif
+
+       read(namelist_buffer, nml=problem_options, iostat=nml_error, iomsg=ioerror_msg)
+       if (nml_error /= 0) then
+          write(*,*)'ERROR: Unable to read "problem_options" in namelist file '
+          call exit(-1)
+       endif
+
+       close(nml_unit)
+    endif
+
+  end subroutine read_namelist_file
+
+  !------------------------------------------------------------------------
+  subroutine read_command_options()
+    !
+    implicit none
+    !
+    PetscBool      :: flg
+    PetscErrorCode :: ierr
+
+    call PetscOptionsGetInt(PETSC_NULL_OPTIONS,PETSC_NULL_CHARACTER,'-ncair',ncair,flg,ierr)
+    call PetscOptionsGetInt(PETSC_NULL_OPTIONS,PETSC_NULL_CHARACTER,'-ntree',ntree,flg,ierr)
+
+  end subroutine read_command_options
+
+  !------------------------------------------------------------------------
+  subroutine run_ml_model_problem(namelist_filename)
+    !
+    implicit none
+    !
+    PetscReal                    :: dt
+    PetscBool                    :: converged
+    PetscInt                     :: istep
+    PetscInt                     :: converged_reason
+    character(len=256), optional :: namelist_filename
+
+    ncair = 1;
+    ntree = 1;
+
+    call read_command_options()
+    call read_namelist_file(namelist_filename)
+
+  end subroutine run_ml_model_problem
+
+end module ml_model_problem
