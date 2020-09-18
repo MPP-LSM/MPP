6928ef1b467b1cefe0dd8a856255f191b8557080
Gautam Bisht <gautam.bisht@pnnl.gov>
2020-09-18 11:06:09 -0700

    Adds a check on number of auxvars

--- src/mpp/ge/GoveqnCanopyAirTemperatureType.F90
+++ src/mpp/ge/GoveqnCanopyAirTemperatureType.F90
@@ -366,6 +366,10 @@ contains
     PetscInt                     :: nauxvar
     PetscReal, pointer           :: var_values(:)
 
+    if (nauxvar > this%mesh%ncells_all) then
+      call endrun(msg="ERROR nauxvar exceeds the number of cells in the mesh "//errmsg(__FILE__, __LINE__))
+    endif
+
     select case(auxvar_type)
     case(AUXVAR_INTERNAL)
        call CAirTempGetRValuesFromAuxVars(this%aux_vars_in, var_type, nauxvar, var_values)
@@ -397,6 +401,10 @@ contains
     PetscInt                     :: geq_leaf_temp_rank
     PetscInt                     :: nauxvar
     PetscReal, pointer           :: var_values(:)
+
+    if (nauxvar > this%mesh%ncells_all) then
+      call endrun(msg="ERROR nauxvar exceeds the number of cells in the mesh "//errmsg(__FILE__, __LINE__))
+    endif
 
     select case(auxvar_type)
     case(AUXVAR_INTERNAL)
--- src/mpp/ge/GoveqnCanopyAirVaporType.F90
+++ src/mpp/ge/GoveqnCanopyAirVaporType.F90
@@ -341,6 +341,10 @@ contains
     PetscInt                      :: nauxvar
     PetscReal, pointer            :: var_values(:)
 
+    if (nauxvar > this%mesh%ncells_all) then
+      call endrun(msg="ERROR nauxvar exceeds the number of cells in the mesh "//errmsg(__FILE__, __LINE__))
+    endif
+
     select case(auxvar_type)
     case(AUXVAR_INTERNAL)
        call CAirVaporGetRValuesFromAuxVars(this%aux_vars_in, var_type, nauxvar, var_values)
@@ -372,6 +376,10 @@ contains
     PetscInt                      :: geq_leaf_temp_rank
     PetscInt                      :: nauxvar
     PetscReal, pointer            :: var_values(:)
+
+    if (nauxvar > this%mesh%ncells_all) then
+      call endrun(msg="ERROR nauxvar exceeds the number of cells in the mesh "//errmsg(__FILE__, __LINE__))
+    endif
 
     select case(auxvar_type)
     case(AUXVAR_INTERNAL)
