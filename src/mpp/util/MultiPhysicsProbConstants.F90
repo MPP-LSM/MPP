cfdc7e34efa2774cb1cf1f905ec5e4303223499f
Gautam Bisht <gautam.bisht@pnnl.gov>
2020-12-19 10:04:40 -0800

    Declares variable for shortwave radiation

--- src/mpp/util/MultiPhysicsProbConstants.F90
+++ src/mpp/util/MultiPhysicsProbConstants.F90
@@ -151,8 +151,8 @@ module MultiPhysicsProbConstants
   PetscInt, parameter, public :: VAR_STOMATAL_CONDUCTANCE_BBERRY   = 654
   PetscInt, parameter, public :: VAR_PHOTOSYNTHETIC_PATHWAY_C4     = 655
   PetscInt, parameter, public :: VAR_PHOTOSYNTHETIC_PATHWAY_C3     = 656
-
-
+  PetscInt, parameter, public :: VAR_SHRTWAVE_ABSORBED_RAD_LEAF    = 657
+  PetscInt, parameter, public :: VAR_SHRTWAVE_ABSORBED_RAD_SOIL    = 658
 
   !
   PetscInt, parameter, public :: AUXVAR_INTERNAL                   = 701
