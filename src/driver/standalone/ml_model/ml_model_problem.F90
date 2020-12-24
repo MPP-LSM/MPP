7ab7904ecbcffdba10f38bab74f9b05915aed61a
Gautam Bisht <gautam.bisht@pnnl.gov>
2020-12-23 17:44:40 -0800

    Moves the subroutine to extract data from MLC model

--- src/driver/standalone/ml_model/ml_model_problem.F90
+++ src/driver/standalone/ml_model/ml_model_problem.F90
@@ -164,7 +164,7 @@ contains
     !
     use ml_model_boundary_conditions , only : read_boundary_conditions
     use ml_model_utils               , only : compute_dpai_fssh
-    use ml_model_utils               , only : extract_data_from_mlc
+    use mlc                          , only : extract_data_from_mlc
     use ml_model_utils               , only : extract_data_from_swv
     use ml_model_utils               , only : extract_data_from_lbl
     use ml_model_utils               , only : extract_data_from_lwv
