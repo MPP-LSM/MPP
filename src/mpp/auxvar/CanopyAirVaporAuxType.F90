dcc984a336d56b62c1facca324e7f2182575085e
Gautam Bisht <gautam.bisht@pnnl.gov>
2021-07-23 14:25:41 -0700

    Adds computation of transpiration flux

--- src/mpp/auxvar/CanopyAirVaporAuxType.F90
+++ src/mpp/auxvar/CanopyAirVaporAuxType.F90
@@ -19,7 +19,7 @@ module CanopyAirVaporAuxType
      ! primary unknown independent variable
      PetscReal          :: qair                ! Water vapor (mol/mol)
 
-     PetscReal          :: qair_prev           ! Water vapor for previous timestep (mol/mol)
+     PetscReal          :: qair_prev           ! Water vapor from previous timestep (mol/mol)
      PetscReal          :: temperature         ! Air temperature (K)
      PetscReal          :: cpair               ! Specific heat of air at constant pressure (J/mol/K)
      PetscReal          :: rhomol              ! Molar density (mol/m3)
@@ -34,16 +34,19 @@ module CanopyAirVaporAuxType
      PetscReal          :: soil_temperature    ! Soil temperature (K)
 
      PetscInt           :: num_leaves
-     PetscReal, pointer :: gbv(:)              ! Leaf boundary layer conductance, H2O (mol H2O/m2 leaf/s)
-     PetscReal, pointer :: leaf_temperature(:) ! Vegetation temperature from previous timestep (K)
-     PetscReal, pointer :: leaf_gs(:)          ! Leaf stomatal conductance (mol H2O/m2 leaf/s)
-     PetscReal, pointer :: leaf_fwet(:)        ! Fraction of plant area index that is wet
-     PetscReal, pointer :: leaf_fdry(:)        ! Fraction of plant area index that is green and dry
-     PetscReal, pointer :: leaf_fssh(:)        ! Sunlit or shaded fraction of canopy layer
-     PetscReal, pointer :: leaf_dpai(:)        ! Layer plant area index (m2/m2)
+     PetscReal, pointer :: gbv(:)                   ! Leaf boundary layer conductance, H2O (mol H2O/m2 leaf/s)
+     PetscReal, pointer :: leaf_temperature(:)      ! Vegetation temperature (K)
+     PetscReal, pointer :: leaf_temperature_prev(:) ! Vegetation temperature from previous timestep (K)
+     PetscReal, pointer :: leaf_gs(:)               ! Leaf stomatal conductance (mol H2O/m2 leaf/s)
+     PetscReal, pointer :: leaf_fwet(:)             ! Fraction of plant area index that is wet
+     PetscReal, pointer :: leaf_fdry(:)             ! Fraction of plant area index that is green and dry
+     PetscReal, pointer :: leaf_fssh(:)             ! Sunlit or shaded fraction of canopy layer
+     PetscReal, pointer :: leaf_dpai(:)             ! Layer plant area index (m2/m2)
+     PetscReal, pointer :: leaf_trans_flux(:)       ! Leaf transpiration flux (mol H2O/m2 leaf/s)
    contains
      procedure, public :: Init     => CAirVaporAuxVarInit
      procedure, public :: PreSolve => CAirVaporAuxVarPreSolve
+     procedure, public :: PostSolve=> CAirVaporAuxVarPostSolve
   end type cair_vapor_auxvar_type
 
 contains
@@ -60,13 +63,15 @@ contains
     PetscInt :: num_leaves
 
     this%num_leaves = num_leaves
-    allocate(this%gbv              (num_leaves))
-    allocate(this%leaf_temperature (num_leaves))
-    allocate(this%leaf_gs          (num_leaves))
-    allocate(this%leaf_fwet        (num_leaves))
-    allocate(this%leaf_fdry        (num_leaves))
-    allocate(this%leaf_fssh        (num_leaves))
-    allocate(this%leaf_dpai        (num_leaves))
+    allocate(this%gbv                   (num_leaves))
+    allocate(this%leaf_temperature      (num_leaves))
+    allocate(this%leaf_temperature_prev (num_leaves))
+    allocate(this%leaf_gs               (num_leaves))
+    allocate(this%leaf_fwet             (num_leaves))
+    allocate(this%leaf_fdry             (num_leaves))
+    allocate(this%leaf_fssh             (num_leaves))
+    allocate(this%leaf_dpai             (num_leaves))
+    allocate(this%leaf_trans_flux       (num_leaves))
 
     this%qair                = 0.d0
 
@@ -104,8 +109,50 @@ contains
     class(cair_vapor_auxvar_type)   :: this
 
     this%qair_prev = this%qair
+    this%leaf_temperature_prev(:) = this%leaf_temperature(:)
 
   end subroutine CAirVaporAuxVarPreSolve
+
+  !------------------------------------------------------------------------
+  subroutine CAirVaporAuxVarPostSolve(this)
+    !
+    ! !DESCRIPTION:
+    ! Make a copy of solution from previous timestep
+    !
+    use WaterVaporMod             , only : SatVap
+    !
+    implicit none
+    !
+    ! !ARGUMENTS
+    class(cair_vapor_auxvar_type)   :: this
+    !
+    PetscReal :: esat, qsat, desat, dqsat
+    PetscReal :: gleaf, gleaf_et, delta
+    PetscInt  :: ileaf
+
+    do ileaf = 1, this%num_leaves
+       if (this%leaf_dpai(ileaf) > 0.d0) then
+          call SatVap(this%leaf_temperature_prev(ileaf), esat, desat)
+
+          qsat = esat /this%pref
+          dqsat= desat/this%pref
+
+          gleaf = &
+               this%leaf_gs(ileaf)  * this%gbv(ileaf)/ &
+               (this%leaf_gs(ileaf) + this%gbv(ileaf))
+
+          gleaf_et = &
+               gleaf           * this%leaf_fdry(ileaf) + &
+               this%gbv(ileaf) * this%leaf_fwet(ileaf)
+
+          delta = qsat + dqsat * (this%leaf_temperature(ileaf) - this%leaf_temperature_prev(ileaf)) - this%qair
+
+          this%leaf_trans_flux(ileaf) = gleaf_et * delta
+       end if
+    end do
+
+  end subroutine CAirVaporAuxVarPostSolve
+
 #endif
 
 end module CanopyAirVaporAuxType
