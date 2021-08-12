52b560583b76030686bd8fc115101884b9f0681b
Gautam Bisht <gautam.bisht@pnnl.gov>
2021-08-12 10:56:36 -0700

    Splits a subroutine into two

--- src/driver/standalone/ml_model/ml_model_boundary_conditions.F90
+++ src/driver/standalone/ml_model/ml_model_boundary_conditions.F90
@@ -3,7 +3,6 @@ module ml_model_boundary_conditions
   use mpp_varctl      , only : iulog
   use mpp_abortutils  , only : endrun
   use mpp_shr_log_mod , only : errMsg => shr_log_errMsg
-  use ml_model_global_vars
   !
   implicit none
   !
@@ -17,74 +16,96 @@ contains
   !------------------------------------------------------------------------
   subroutine allocate_memory()
     !
-    use ml_model_utils      , only : allocate_memory_for_condition
-    use ml_model_meshes     , only : nleaf
-    !
     implicit none
 
-    call allocate_memory_for_condition(bnd_cond%Iskyb_vis , ncair)
-    call allocate_memory_for_condition(bnd_cond%Iskyd_vis , ncair)
-    call allocate_memory_for_condition(bnd_cond%Iskyb_nir , ncair)
-    call allocate_memory_for_condition(bnd_cond%Iskyd_nir , ncair)
-    call allocate_memory_for_condition(bnd_cond%Irsky     , ncair)
-
-    call allocate_memory_for_condition(bnd_cond%tref  , ncair)
-    call allocate_memory_for_condition(bnd_cond%qref  , ncair)
-    call allocate_memory_for_condition(bnd_cond%pref  , ncair)
-
-    call allocate_memory_for_condition(bnd_cond%co2ref , ncair)
-    call allocate_memory_for_condition(bnd_cond%o2ref , ncair)
-
-    call allocate_memory_for_condition(bnd_cond%uref  , ncair)
-
-    call allocate_memory_for_condition(bnd_cond%Albsoib_vis, ncair)
-    call allocate_memory_for_condition(bnd_cond%Albsoib_nir, ncair)
-    call allocate_memory_for_condition(bnd_cond%Albsoid_vis, ncair)
-    call allocate_memory_for_condition(bnd_cond%Albsoid_nir, ncair)
-
-    call allocate_memory_for_condition(bnd_cond%tg, ncair)
-    call allocate_memory_for_condition(bnd_cond%soil_t, ncair)
-
-    call allocate_memory_for_condition(bnd_cond%sza, ncair)
-
-    call allocate_memory_for_condition(bnd_cond%rhg, ncair)
-    call allocate_memory_for_condition(bnd_cond%soilres, ncair)
-    call allocate_memory_for_condition(bnd_cond%soil_tk, ncair)
-    call allocate_memory_for_condition(bnd_cond%h2osoi_vol, ncair*10)
-
-    call allocate_memory_for_condition(bnd_cond%pref_prev, ncair)
-
-    call allocate_memory_for_condition(bnd_cond%fwet, ncair*ntree*(ntop-nbot+1)*nleaf)
-    call allocate_memory_for_condition(bnd_cond%fdry, ncair*ntree*(ntop-nbot+1)*nleaf)
-
-    call allocate_memory_for_condition(int_cond%gbh , ncair*ntree*(ntop-nbot+1)*nleaf)
-    call allocate_memory_for_condition(int_cond%gbv , ncair*ntree*(ntop-nbot+1)*nleaf)
-    call allocate_memory_for_condition(int_cond%gbc , ncair*ntree*(ntop-nbot+1)*nleaf)
-
-    call allocate_memory_for_condition(int_cond%gs_sun, ncair*ntree*(ntop-nbot+1))
-    call allocate_memory_for_condition(int_cond%gs_shd, ncair*ntree*(ntop-nbot+1))
-
-    call allocate_memory_for_condition(int_cond%Tcan      , ncair)
-
-    call allocate_memory_for_condition(int_cond%Tair      , ncair*nz_cair)
-    call allocate_memory_for_condition(int_cond%Qair      , ncair*nz_cair)
-    call allocate_memory_for_condition(int_cond%Wind      , ncair*nz_cair)
-
-    call allocate_memory_for_condition(int_cond%Tleaf_sun , ncair*(ntop-nbot+1) )
-    call allocate_memory_for_condition(int_cond%Tleaf_shd , ncair*(ntop-nbot+1) )
-    call allocate_memory_for_condition(int_cond%Labs_leaf_sun, ncair*(ntop-nbot+1) )
-    call allocate_memory_for_condition(int_cond%Labs_leaf_shd, ncair*(ntop-nbot+1) )
-    call allocate_memory_for_condition(int_cond%gs_shd    , ncair*(ntop-nbot+1) )
-    call allocate_memory_for_condition(int_cond%Ileaf_sun_vis    , ncair*(ntop-nbot+1) )
-    call allocate_memory_for_condition(int_cond%Ileaf_shd_vis    , ncair*(ntop-nbot+1) )
-    call allocate_memory_for_condition(int_cond%Ileaf_sun_nir    , ncair*(ntop-nbot+1) )
-    call allocate_memory_for_condition(int_cond%Ileaf_shd_nir    , ncair*(ntop-nbot+1) )
-    
-    call allocate_memory_for_condition(int_cond%Labs_soil, ncair)
-    call allocate_memory_for_condition(int_cond%Isoil_vis, ncair)
-    call allocate_memory_for_condition(int_cond%Isoil_nir, ncair)
+    call allocate_memory_for_bc()
+    call allocate_memory_for_ic()
 
   end subroutine allocate_memory
+
+  !------------------------------------------------------------------------
+  subroutine allocate_memory_for_bc()
+    !
+    use ml_model_utils      , only : allocate_memory_for_condition
+    use ml_model_meshes     , only : nleaf
+    use ml_model_global_vars, only : ncair, ntree, ntop, nbot, nz_cair, bnd_cond
+    !
+    implicit none
+    !
+    call allocate_memory_for_condition(bnd_cond%Iskyb_vis   , ncair)
+    call allocate_memory_for_condition(bnd_cond%Iskyd_vis   , ncair)
+    call allocate_memory_for_condition(bnd_cond%Iskyb_nir   , ncair)
+    call allocate_memory_for_condition(bnd_cond%Iskyd_nir   , ncair)
+    call allocate_memory_for_condition(bnd_cond%Irsky       , ncair)
+
+    call allocate_memory_for_condition(bnd_cond%tref        , ncair)
+    call allocate_memory_for_condition(bnd_cond%qref        , ncair)
+    call allocate_memory_for_condition(bnd_cond%pref        , ncair)
+
+    call allocate_memory_for_condition(bnd_cond%co2ref      , ncair)
+    call allocate_memory_for_condition(bnd_cond%o2ref       , ncair)
+
+    call allocate_memory_for_condition(bnd_cond%uref        , ncair)
+
+    call allocate_memory_for_condition(bnd_cond%Albsoib_vis , ncair)
+    call allocate_memory_for_condition(bnd_cond%Albsoib_nir , ncair)
+    call allocate_memory_for_condition(bnd_cond%Albsoid_vis , ncair)
+    call allocate_memory_for_condition(bnd_cond%Albsoid_nir , ncair)
+
+    call allocate_memory_for_condition(bnd_cond%tg          , ncair)
+    call allocate_memory_for_condition(bnd_cond%soil_t      , ncair)
+
+    call allocate_memory_for_condition(bnd_cond%sza         , ncair)
+
+    call allocate_memory_for_condition(bnd_cond%rhg         , ncair)
+    call allocate_memory_for_condition(bnd_cond%soilres     , ncair)
+    call allocate_memory_for_condition(bnd_cond%soil_tk     , ncair)
+    call allocate_memory_for_condition(bnd_cond%h2osoi_vol  , ncair*10)
+
+    call allocate_memory_for_condition(bnd_cond%pref_prev   , ncair)
+
+    call allocate_memory_for_condition(bnd_cond%fwet        , ncair*ntree*(ntop-nbot+1)*nleaf)
+    call allocate_memory_for_condition(bnd_cond%fdry        , ncair*ntree*(ntop-nbot+1)*nleaf)
+
+  end subroutine allocate_memory_for_bc
+
+  !------------------------------------------------------------------------
+  subroutine allocate_memory_for_ic()
+    !
+    use ml_model_utils      , only : allocate_memory_for_condition
+    use ml_model_meshes     , only : nleaf
+    use ml_model_global_vars, only : ncair, ntree, ntop, nbot, nz_cair, int_cond
+    !
+    implicit none
+    !
+    call allocate_memory_for_condition(int_cond%gbh           , ncair*ntree*(ntop-nbot+1)*nleaf)
+    call allocate_memory_for_condition(int_cond%gbv           , ncair*ntree*(ntop-nbot+1)*nleaf)
+    call allocate_memory_for_condition(int_cond%gbc           , ncair*ntree*(ntop-nbot+1)*nleaf)
+
+    call allocate_memory_for_condition(int_cond%gs_sun        , ncair*ntree*(ntop-nbot+1))
+    call allocate_memory_for_condition(int_cond%gs_shd        , ncair*ntree*(ntop-nbot+1))
+
+    call allocate_memory_for_condition(int_cond%Tcan          , ncair)
+
+    call allocate_memory_for_condition(int_cond%Tair          , ncair*nz_cair)
+    call allocate_memory_for_condition(int_cond%Qair          , ncair*nz_cair)
+    call allocate_memory_for_condition(int_cond%Wind          , ncair*nz_cair)
+
+    call allocate_memory_for_condition(int_cond%Tleaf_sun     , ncair*(ntop-nbot+1) )
+    call allocate_memory_for_condition(int_cond%Tleaf_shd     , ncair*(ntop-nbot+1) )
+    call allocate_memory_for_condition(int_cond%Labs_leaf_sun , ncair*(ntop-nbot+1) )
+    call allocate_memory_for_condition(int_cond%Labs_leaf_shd , ncair*(ntop-nbot+1) )
+    call allocate_memory_for_condition(int_cond%gs_shd        , ncair*(ntop-nbot+1) )
+    call allocate_memory_for_condition(int_cond%Ileaf_sun_vis , ncair*(ntop-nbot+1) )
+    call allocate_memory_for_condition(int_cond%Ileaf_shd_vis , ncair*(ntop-nbot+1) )
+    call allocate_memory_for_condition(int_cond%Ileaf_sun_nir , ncair*(ntop-nbot+1) )
+    call allocate_memory_for_condition(int_cond%Ileaf_shd_nir , ncair*(ntop-nbot+1) )
+    
+    call allocate_memory_for_condition(int_cond%Labs_soil     , ncair)
+    call allocate_memory_for_condition(int_cond%Isoil_vis     , ncair)
+    call allocate_memory_for_condition(int_cond%Isoil_nir     , ncair)
+
+  end subroutine allocate_memory_for_ic
 
   !------------------------------------------------------------------------
   subroutine read_boundary_conditions(istep, bc_data)
